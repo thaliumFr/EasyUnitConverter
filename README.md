32b7f0bfdeb70eff66bb252cfde376df6cc189e1
thalium <theomaxmi34@gmail.com>
2022-08-24 01:53:25 +0200

    - Merge Clipboard.py and main.py - Updated Actions - Added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# Easy Unit Converter
+
+Getting mad because of Metrics and Imperials? just press 2 key and you're all set wihth that program, no need of Google or any internet access!
+
+## How does it works?
+
+- Select the text composed of the value and the unit: _67m_, _151 °f_, ...
+- Press **ctrl + F8** and a popup will appear telling you the value in the corresponding unit in the other unit system
+
+## Wanna help?
+
+All the brains available are welcomed to share their knowledge.
+
+- Fork the project
+- Do your changes
+- Pull request
+- Wait for review
+
+## What can I do?
+
+- Documenting
+- Fix bugs from the **Issues** tab
+- Optimization
+- Add more units support
--- src/main.py
+++ src/main.py
@@ -2,13 +2,22 @@ from time import sleep
 from keyboard import is_pressed
 import re;
 from tkinter.messagebox import *
+import pyperclip
+import pyautogui as pya
 
 from Units import *
-from Clipboard import *
 
-def SplitNumStr(s:str) -> list[str]:
-    res:list[str] = re.split('([-+]?\d+\.\d+)|([-+]?\d+)', s.strip())
-    res_f:list[str] = [r.strip() for r in res if r is not None and r.strip() != '']
+hotkey = "ctrl + F8"
+
+def copy_clipboard() -> str:
+    pyperclip.copy("") # <- This prevents last copy replacing current copy of null.
+    pya.hotkey('ctrl', 'c')
+    sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
+    return pyperclip.paste()
+
+def SplitNumStr(st:str) -> list[str]:
+    res = re.split('([-+]?\d+\.\d+)|([-+]?\d+)', st.strip())
+    res_f = [r.strip() for r in res if r is not None and r.strip() != '']
     return res_f
 
 if __name__ == "__main__":
